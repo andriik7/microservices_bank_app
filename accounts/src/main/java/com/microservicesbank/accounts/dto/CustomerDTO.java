b3c5e82d6226540c2248991199566f3f1f6e9fc1
Andrii Kuchera <akuchera7@gmail.com>
2024-09-26 17:40:56 +0300

    Create documentation with OpenAPI
    Here I add dependency to use OpenAPI to write detailed documentation for account microservice, adding examples, names and descriptions where it is needed

--- accounts/src/main/java/com/microservicesbank/accounts/dto/CustomerDTO.java
+++ accounts/src/main/java/com/microservicesbank/accounts/dto/CustomerDTO.java
@@ -1,25 +1,45 @@
 package com.microservicesbank.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
+@Schema(
+        name = "Customer",
+        description = "Schema to store Customer and Account details"
+)
 @Data
 public class CustomerDTO {
 
+    @Schema(
+            description = "Name of the customer",
+            example = "John Doe"
+    )
     @NotEmpty(message = "Name cannot be empty")
     @Size(min = 5, max = 30, message = "Name must be between 5 and 30 characters")
     private String name;
 
+    @Schema(
+            description = "Email address of the customer",
+            example = "ak47.10.07.06@gmail.com"
+    )
     @NotEmpty(message = "Email cannot be empty")
     @Email(message = "Invalid email address")
     private String email;
 
+    @Schema(
+            description = "Mobile number of the customer",
+            example = "0666992283"
+    )
     @NotEmpty
     @Pattern(regexp = "^\\d{10}$", message = "Invalid mobile number")
     private String mobileNumber;
 
+    @Schema(
+            description = "Account details"
+    )
     private AccountDTO accountDto;
 }
