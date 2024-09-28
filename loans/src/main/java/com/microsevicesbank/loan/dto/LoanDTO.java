27944136de873acbaef1a273c1b048de580cf124
Andrii Kuchera <akuchera7@gmail.com>
2024-09-28 18:34:07 +0300

    TDD for LoanService with LoanServiceTest
    Create the most important classes for the service to work properly. Using TDD, I write tests for every case of every method in LoanService. Mapper and other classes are now added and fixed.

--- loans/src/main/java/com/microsevicesbank/loan/dto/LoanDTO.java
+++ loans/src/main/java/com/microsevicesbank/loan/dto/LoanDTO.java
@@ -3,34 +3,53 @@ package com.microsevicesbank.loan.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 @Schema(
-        name = "Card",
-        description = "Schema to store card details"
+        name = "Loan",
+        description = "Schema to store loan details"
 )
 @Data
 public class LoanDTO {
 
+    @NotEmpty(message = "Mobile Number cannot be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile Number must be 10 digits")
     @Schema(
-            description = "Account number of bank account",
-            example = "1234567890"
+            description = "Mobile Number of Customer", example = "4365327698"
     )
-    @NotEmpty(message = "Account number cannot be empty")
-    @Pattern(regexp = "^\\d{10}$", message = "Account number must be 10 digits")
-    private Long accountNumber;
+    private String mobileNumber;
 
+    @NotEmpty(message = "Loan Number cannot be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{16})", message = "LoanNumber must be 16 digits")
     @Schema(
-            description = "Account type of bank account",
-            example = "Savings"
+            description = "Loan Number of the customer", example = "1234123412341234"
     )
-    @NotEmpty(message = "Account type cannot be empty")
-    private String accountType;
+    private String loanNumber;
 
+    @NotEmpty(message = "LoanType cannot be null or empty")
     @Schema(
-            description = "Branch address of bank account",
-            example = "123 Main Street, New York"
+            description = "Type of the loan", example = "Home Loan"
     )
-    @NotEmpty(message = "Branch address cannot be empty")
-    private String branchAddress;
+    private String loanType;
+
+    @Positive(message = "Total loan amount should be greater than 0")
+    @Schema(
+            description = "Total loan amount", example = "100000"
+    )
+    private int totalLoan;
+
+    @PositiveOrZero(message = "Total loan amount paid should be equal or greater than 0")
+    @Schema(
+            description = "Total loan amount paid", example = "1000"
+    )
+    private int amountPaid;
+
+    @PositiveOrZero(message = "Total outstanding amount should be equal or greater than 0")
+    @Schema(
+            description = "Total outstanding amount against a loan", example = "99000"
+    )
+    private int outstandingAmount;
+
 }
