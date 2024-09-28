8d849898962d11f08dd69efe7dfbe06a71ad9439
Andrii Kuchera <akuchera7@gmail.com>
2024-09-28 15:57:14 +0300

    Cards and Loans basic setup
    Configure some basic settings, especially in properties.yml. Make sure all basic classes are  working properly and all microservices are running on different ports. Loans are already set up, including denpendencies

--- loans/src/main/java/com/microsevicesbank/loan/model/Loan.java
+++ loans/src/main/java/com/microsevicesbank/loan/model/Loan.java
@@ -1,4 +1,29 @@
 package com.microsevicesbank.loan.model;
 
-public class Loan {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Loan extends SuperEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+    private String mobileNumber;
+
+    private String loanNumber;
+
+    private String loanType;
+
+    private int totalLoan;
+
+    private int amountPaid;
+
+    private int outstandingAmount;
+
 }
