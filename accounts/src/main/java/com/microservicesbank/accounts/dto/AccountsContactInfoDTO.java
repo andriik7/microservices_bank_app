94ba504a1315674266fb0fb2261ae4e2c87b20f4
Andrii Kuchera <akuchera7@gmail.com>
2024-10-04 14:46:01 +0300

    Update config server and microservices to support instant updates of config files connected through GitHub. Here are implemented 2 ways: 1. Use actuator/refresh endpoint for each microservice 2. Use actuator/busrefresh endpoint that uses RabbitMQ to update all the configurations at once.

--- accounts/src/main/java/com/microservicesbank/accounts/dto/AccountsContactInfoDTO.java
+++ accounts/src/main/java/com/microservicesbank/accounts/dto/AccountsContactInfoDTO.java
@@ -1,10 +1,19 @@
 package com.microservicesbank.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountsContactInfoDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
 }
--- cards/src/main/java/com/microsevicesbank/card/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/microsevicesbank/card/dto/CardsContactInfoDTO.java
@@ -1,10 +1,19 @@
 package com.microsevicesbank.card.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class CardsContactInfoDTO {
+
+    private String message;
+
+    private Map<String, String> contactDetails;
+
+    private List<String> onCallSupport;
 }
