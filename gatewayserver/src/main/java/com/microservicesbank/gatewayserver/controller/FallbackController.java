154f076eeb2bad4fc0bebbe447c99368a33d01f0
Andrii Kuchera <akuchera7@gmail.com>
2024-10-14 15:56:44 +0300

    Add fallback URI
    Configure Gateway Server main class mentioning there previously created falback URI to redirect client in case of failure.

---
+++ gatewayserver/src/main/java/com/microservicesbank/gatewayserver/controller/FallbackController.java
@@ -0,0 +1,15 @@
+package com.microservicesbank.gatewayserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/contactSupport")
+    public Mono<String> contactSupport() {
+        return Mono.just("An error occurred. Please try again later or contact support!");
+    }
+
+}
