bd81e57f041e22f7741a482a43a1876f82bcf7de
Andrii Kuchera <akuchera7@gmail.com>
2024-10-16 18:45:40 +0300

    Implement Rate limiter pattern form Resilience4j
    Here I'am providing rate limiter implementation in gateway server using Redis, KeyResolver and routes configurations. Also I provide custom rate limiter for accounts microservice's CustomerContoller's method along with @Retry annotation(they can't work together). In .yml config for an accounts microservice there are specified and explained timeoutDuration, limitRefreshPeriod and limitForPeriod, falback method is set up as well.

--- gatewayserver/src/main/java/com/microservicesbank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/microservicesbank/gatewayserver/GatewayserverApplication.java
@@ -7,10 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -50,7 +53,9 @@ public class GatewayserverApplication {
                                    .uri("lb://CARDS"))
                       .route(r -> r.path("/microbank/loans/**")
                                    .filters(f -> f.rewritePath("/microbank/loans/(?<segment>.*)", "/${segment}")
-                                                  .addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+                                                  .addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+                                                  .requestRateLimiter(config -> config.setRateLimiter(redisRateLimiter())
+                                                          .setKeyResolver(userKeyResolver())))
 //                                                  .circuitBreaker(config -> config.setName("loansCircuitBreaker")
 //                                                          .setFallbackUri("forward:/fallback/contactSupport"))
                                    .uri("lb://LOANS"))
@@ -64,5 +69,16 @@ public class GatewayserverApplication {
                     .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
                     .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(5)).build()).build() // 5 second default timeout for circuit breaker
         );
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("user"))
+                .defaultIfEmpty("anonymous");
     }
 }
